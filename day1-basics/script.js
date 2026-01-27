d92d938fbb58e7b96f1d10ddc43b7c8bcc95a794
sani <sunnyphauzdar6@gmail.com>
2026-01-27 21:04:35 +0530

    Add files via upload

--- day1-basics/script.js
+++ day1-basics/script.js
@@ -1,1 +1,35 @@
+console.log("hello world");
+ 
+  //practice
+//1
+ let a=10;
+  let b=30;
+  console.log(a+b); //40
 
+  //2
+  const myName = "sunny kuntal";
+  console.log("my name is ", myName);
+ //3
+ let x=5;
+ x=10;
+ console.log(x);
+
+ //4
+ let price=100;
+ let gst=18;
+ console.log(price+gst);
+ 
+ //5
+ let isStudent=true;
+ console.log(isStudent);
+
+ //mini task
+  
+ let myAge=23;
+ let myGoal="web developer";
+ console.log( myName);
+ console.log(myAge);
+ console.log(myGoal);
+let isMarried=false;
+console.log(isMarried); 
+ 
