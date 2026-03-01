a58753f1a6b61640690ef4da0c1dece49f6f3ca6
sani <sunnyphauzdar6@gmail.com>
2026-03-01 14:56:56 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,103 @@
+  /**
+  //practice of learning javascript
+  //day 1 
+  // Basic JavaScript: Use the Console.
+  // Use the console.log() method to print the value of the variable to the console.
+  //example
+
+   console.log("hello sunnykuntal")
+
+  //variable:where the data is stored.
+  
+  //example name
+let name = "sunnykuntal"
+console.log(name);
+
+//variable
+//let : let is used to declare a variable that can be reassigned later in the code.
+//name : name is the name of the variable that we are declaring. It can be any valid identifier in JavaScript.
+//"sunnykuntal" : "sunnykuntal" is a string value that we are assigning to the variable name. It is enclosed in double quotes to indicate that it is a string literal.
+
+//example age
+let age = 24;
+console.log("age", age);
+
+//example city 
+let cityname = "mathura,uttar pradesh";
+console.log("cityname:", cityname);
+
+// example student
+let isStudent = true;
+console.log("isStudent:", isStudent); 
+
+ //practice of learning javascript
+ //question1
+ let name = "sunnykuntal";
+ const country = "india";
+ //country ="USA"; //ERROR;
+ let age = 24;
+ const isStudent = true;
+
+ console.log("name:", name); //name: sunnykuntal
+ console.log("country:", country); //country: india
+ console.log("age:", age); //age: 24
+ console.log("isStudent:", isStudent); //isStudent: true 
+**/
+
+
+ //QUESTION 1
+ // Create a variable called "greeting" and assign it the value "Hello, World!".
+ let greeting= "Hello, World!";
+  console.log(greeting); //Hello, World! 
+
+
+//QUESTION 2
+// add your name in name variable and print it in console;
+let name = "sunnykuntal";
+console.log("name :",name) //name : sunnykuntal;
+
+//QUESTION 3
+// Create a variable called "age" and assign it your age. Then, print the value of "age" to the console.
+let age = 24;
+console.log("age:", age); //age: 24
+
+//QUESTION 4
+// Create a variable called "isStudent" and assign it a boolean value indicating whether you are a student or not. Then, print the value of "isStudent" to the console.
+let isStudent = true;
+console.log("isStudent:", isStudent); //isStudent: true
+
+//QUESTION 5
+// Create a variable called "city" and assign it the name of the city you live in. Then, print the value of "city" to the console.
+let city = "mathura,uttar pradesh";
+console.log("city:", city); //city: mathura,uttar pradesh 
+
+//QUESTION 6
+// Create a variable called "favoriteColor" and assign it your favorite color. Then, print the value of "favoriteColor" to the console.
+let favoriteColor = "blue";
+console.log("favoriteColor:", favoriteColor); //favoriteColor: blue
+
+//QUESTION 7
+// create number variable and print it in console then change the value of number variable and print it again in console;
+let number = 10;
+console.log("number:", number); //number: 10
+number = 20;
+console.log("number:", number); //number: 20
+
+//QUESTION 8
+//create  a const variable and try to change its value then print it in console;
+const country = "india";
+//country = "USA"; //ERROR: Assignment to constant variable.
+console.log( country); //country: india
+
+//QUESTION 9
+//string length
+let str = "Hello, World!";
+let length = str.length;
+console.log("length of string:", length); //length of string: 13
+
+//QUESTION 10
+//string concatenation
+let firstName = "Sunny";
+let lastName = "Kuntal";
+let fullName = firstName + " " + lastName;
+console.log("fullName:", fullName); //fullName: Sunny Kuntal
